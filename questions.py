d4ad77ca385ae4c58666bc38a5cc843a266a24b9
Emilio Pablo Cerrate <epcerrate@gmail.com>
2025-03-21 16:49:57 -0300

    Modifico questions.py para no acceder a las listas usando índices.

--- questions.py
+++ questions.py
@@ -27,14 +27,15 @@ correct_answers_index = [1, 2, 0, 3, 1]
 # Inicializamos el puntaje del usuario en 0
 score = 0
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+# Creamos una lista de tuplas donde cada una contiene una pregunta, sus posibles respuestas y
+# el índice de la respuesta correcta. Luego, seleccionamos 3 aleatoriamente las guardamos en una nueva lista
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 
+# El usuario deberá contestar 3 preguntas
+for question, possible_answers, correct_answer_index in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question)
+    for i, answer in enumerate(possible_answers):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -52,7 +53,7 @@ for _ in range(3):
 
         # Se verifica si la respuesta es correcta, y si lo es,
         # se suma 1 punto al puntaje
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_answer_index:
             score += 1
             print("¡Correcto!")
             break
@@ -64,7 +65,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(possible_answers[correct_answer_index])
 
     # Se imprime un blanco al final de la pregunta
     print()
